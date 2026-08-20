e2a4444763cfc7208a9eb76be0689e3c61c6e94a
Ashupalve <ashwedp@gmail.com>
2026-08-20 23:00:37 +0530

    Q3 done of day 1

---
+++ 30_Day_Python_Challenge/Day 1/03.py
@@ -0,0 +1,10 @@
+# Write a program to find the area and perimeter of a rectangle given its length and width
+
+length = float (input ("Enter the length of the rectangle :"))
+width = float (input ("Enter the width of the rectangle :"))
+
+area =  length * width
+perimeter = 2*(length + width )
+
+print (f"Area of given rectangle is : {area} \nPerimeter of given rectangle is : {perimeter}")
+print ("Thank you for using this programm")
