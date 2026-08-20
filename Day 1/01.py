400ee60d381d1634d39eb884b0dc55a2e25e1903
Ashupalve <ashwedp@gmail.com>
2026-08-20 22:35:58 +0530

    Day 1 of my python 30 days challenge and its 1st question is done

---
+++ Day 1/01.py
@@ -0,0 +1,5 @@
+# Write a program to print "Hello, World!" and then print your name on the next line
+
+print("Hello, World!")
+name = "Ashwed Subhash Plave " 
+print(f"Iam {name} i taking this 30 day python program challenge for improving my skills ")
