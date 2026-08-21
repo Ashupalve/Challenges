eca06d483d94d5fb2008fdd4c3303600fa3cdc49
Ashupalve <ashwedp@gmail.com>
2026-08-21 15:10:19 +0530

    Q1 of day 2 is done

---
+++ 30_Day_Python_Challenge/Day2/01.py
@@ -0,0 +1,8 @@
+# Write a program to check whether a number entered by the user is even or odd.
+
+n= int (input("Enter a number :"))
+
+if (n/2==0 and n==0):
+    print (f"{n} is even number")
+else:
+    print(f"{n} is odd number ")
