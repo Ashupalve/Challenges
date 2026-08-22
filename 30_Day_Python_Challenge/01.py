2a0d0cc34505b704740dc6abde339247f45bf822
Ashupalve <ashwedp@gmail.com>
2026-08-22 10:48:28 +0530

    Q1 og day 3 is done

---
+++ 30_Day_Python_Challenge/01.py
@@ -0,0 +1,7 @@
+# Write a program to print all numbers from 1 to N using a for loop.
+
+n = int (input (" Enter how many numbers you want to print :"))
+
+for i in range (1 ,n+1):
+    print(i)
+    i=i+1
