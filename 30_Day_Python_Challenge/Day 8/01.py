0942d7403fdd3d8c098ece45fd076432b8034009
Ashupalve <ashwedp@gmail.com>
2026-08-29 23:06:46 +0530

    Q1 of day 8 is done

---
+++ 30_Day_Python_Challenge/Day 8/01.py
@@ -0,0 +1,7 @@
+#  Write a program to implement a function that returns the sum of all elements in a list using recursion.
+
+def recursive_sum(lst):
+    if not lst:
+        return 0
+    return lst[0] + recursive_sum(lst[1:])
+print(recursive_sum([1, 2, 3, 4, 5]))
