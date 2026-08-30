b398227f4df2f125fc0adb618b75e344a37eb2d9
Ashupalve <ashwedp@gmail.com>
2026-08-30 22:41:04 +0530

    Q2 of day 9 is done

---
+++ 30_Day_Python_Challenge/Day 9/02.py
@@ -0,0 +1,6 @@
+# Q2. Write a program to find the intersection and union of two sets.
+
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+print("Union:", set1 | set2)
+print("Intersection:", set1 & set2)
