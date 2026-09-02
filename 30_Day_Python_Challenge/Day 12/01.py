a397fbcbd626f4b716e94cc698e8d1bb10761e9f
Ashupalve <ashwedp@gmail.com>
2026-09-02 08:54:54 +0530

    Q1 done of day 12

---
+++ 30_Day_Python_Challenge/Day 12/01.py
@@ -0,0 +1,12 @@
+# Q1. Write a program to create a class `Student` with attributes name and marks, and a method to display them.
+
+class student:
+    def __init__(self ,name ,marks):
+        self.name = name 
+        self.marks = marks
+
+s1 = student("Ashwed", 85)
+print(f"Name {s1.name} , Marks {s1.marks}")
+name = input("Enter student name :")
+marks = int (input("Enter their marks :"))
+s2 = student(name , marks)
