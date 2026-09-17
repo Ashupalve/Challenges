3b8ab98f296b918e880e736a66c8a88c0537258d
Ashwed Palve <ashwedp@gmail.com>
2026-09-17 23:42:03 +0530

    Q2 done of day 14

--- 30_Day_Python_Challenge/Day 14/01.py
+++ 30_Day_Python_Challenge/Day 14/01.py
@@ -1,3 +1,10 @@
 # Q1. Write a program to implement Linear Search on a list and return the index of the target
 # element
 
+def linear_search(arr, target):
+    for i, val in enumerate(arr):
+        if val == target:
+            return i
+    return -1
+nums = [4, 2, 9, 6, 5]
+print(linear_search(nums, 6))
