45dfe6c0d0f3aaa51732c460b39b32e84be20f9f
Ashwed Palve <ashwedp@gmail.com>
2026-09-18 22:00:58 +0530

    Q3 done of day 14

---
+++ 30_Day_Python_Challenge/Day 14/03.py
@@ -0,0 +1,10 @@
+# Q3. Write a program to implement Bubble Sort on a list of numbers
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        for j in range(n - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+print(bubble_sort([5, 2, 9, 1, 5, 6]))
